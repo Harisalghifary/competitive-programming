bc14f62924ba0f23bc999f0df321cc539d06940a
haris.alghifary <haris.solo68@gmail.com>
2023-01-19 23:59:47 +0700

    Refactor 1880.py

--- leetcode/1880-Check-if-Word-Equals-Summation-of-Two-Words.py
+++ leetcode/1880-Check-if-Word-Equals-Summation-of-Two-Words.py
@@ -4,5 +4,4 @@ def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
         for char in s:
             res+=str((ord(char)-97))
         return int(res)
-    
     return (helper(firstWord)+helper(secondWord))==helper(targetWord)
