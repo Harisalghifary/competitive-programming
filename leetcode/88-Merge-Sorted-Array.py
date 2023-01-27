6248def4fc5162295a5a3570f74e2d7471c7813d
haris.alghifary <haris.solo68@gmail.com>
2023-01-27 23:56:10 +0700

    Change 88-Merge Sorted Array.py

--- leetcode/88-Merge-Sorted-Array.py
+++ leetcode/88-Merge-Sorted-Array.py
@@ -1,18 +1,21 @@
+# Difficulty : Easy
+# Tag : Array, Sort, Two Pointers
 def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
     """
     Do not return anything, modify nums1 in-place instead.
     """
-    idx1, idx2, write_idx = m-1, n-1, m+n-1
-    
-    while idx2>=0:
-        if idx1>=0 and nums1[idx1]>nums2[idx2]:
-            nums1[write_idx]=nums1[idx1]
-            idx1-=1
-        
-        else:
-            nums1[write_idx]=nums2[idx2]
-            idx2-=1
-        
-        write_idx-=1
-                
+    i = m-1
+    j = n-1
+    k = m+n-1
+    # iterate from end
+    while j>=0:
+        if i>=0 and nums1[i]>nums2[j]:
+            nums1[k] = nums1[i]
+            i-=1
+            k-=1
+        else: 
+            # nums1[m] < nums2[n]
+            nums1[k] = nums2[j]
+            j-=1
+            k-=1
     return nums1
