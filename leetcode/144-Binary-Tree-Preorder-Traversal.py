e28a0dcc402d7876dadaa3eac542fc1ff02949f9
Masamune Date <haris.solo68@gmail.com>
2023-06-06 14:50:16 +0700

    Resolve 144-Binary Tree Preorder Traversal.py
    better syntax and clean

--- leetcode/144-Binary-Tree-Preorder-Traversal.py
+++ leetcode/144-Binary-Tree-Preorder-Traversal.py
@@ -7,26 +7,26 @@
 #         self.left = left
 #         self.right = right
 def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-#         result = []
-#         if root:
-#             result.append(root.val)
-#             result = result + self.preorderTraversal(root.left)
-#             result = result + self.preorderTraversal(root.right)
-#         return result
+        # result = []
+        # if root:
+        #     result.append(root.val)
+        #     result = result + self.preorderTraversal(root.left)
+        #     result = result + self.preorderTraversal(root.right)
+        # return result
 
 # iteratively
-    if root is None:
-        return []
-    current = root
-    stack, result = [], []
-    stack.append(current)
+        if not root:
+            return []
+        current = root
+        stack, result = [], []
+        stack.append(current)
 
-    while len(stack)>0:
-        node = stack.pop()
-        result.append(node.val)
-        if node.right is not None:
-            stack.append(node.right)
-        if node.left is not None:
-            stack.append(node.left)
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
 
-    return result
+        return result
