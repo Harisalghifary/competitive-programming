4b1e5928528af64713783bd8db4636e465e1284f
Masamune Date <haris.solo68@gmail.com>
2023-06-10 00:21:57 +0700

    Solve 1318-Minimum-Flips-to-Make-a-OR-b-Equal-to-c.py

---
+++ leetcode/1318-Minimum-Flips-to-Make-a-OR-b-Equal-to-c.py
@@ -0,0 +1,23 @@
+# Difficulty : Medium
+# Tag : Bit Manipulation
+def countNegatives(self, grid: List[List[int]]) -> int:
+    # O(n*m)
+    # negatives = 0
+    # for i in range(len(grid)):
+    #     for j in range(len(grid[0])):
+    #         if grid[i][j] < 0 :
+    #             negatives += 1
+    # return negatives
+
+    # O(n+m)
+    rows, colls = len(grid), len(grid[0])
+    row, col = rows-1, 0
+    negatives = 0
+    while row>=0 and col<colls:
+        if grid[row][col]<0:
+            negatives += colls - col
+            row -= 1
+        else: 
+            col += 1
+
+    return negatives
