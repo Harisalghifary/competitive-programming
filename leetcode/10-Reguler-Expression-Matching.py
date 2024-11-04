19b40352e8f5fcdd56b24db477894591c0c590a8
Masamune Date <haris.solo68@gmail.com>
2024-11-05 01:13:02 +0700

    10: added maps as cache

--- leetcode/10-Reguler-Expression-Matching.py
+++ leetcode/10-Reguler-Expression-Matching.py
@@ -1,8 +1,14 @@
 # Difficulty: Hard
 # Tag: string, dynamic programming, Recursion
 def isMatch(self, s: str, p: str) -> bool:
+    # using cache with maps
+    cache = {}
+
     # TOP-Down recursive
     def dfs(i,j):
+        # check cache
+        if (i,j) in cache:
+            return cache[(i,j)]
         # base case
         if i >= len(s) and j >= len(p):
             return True
@@ -14,13 +20,15 @@ def isMatch(self, s: str, p: str) -> bool:
         # handle *
         if (j+1)<len(p) and p[j+1] == '*':
             # handle for empty (i,j+2) and consecutive (i+1,j)
-            return dfs(i,j+2) or (match and dfs(i+1,j))
+            cache[(i,j)] = dfs(i,j+2) or (match and dfs(i+1,j))
+            return cache[(i,j)]
         # handle for case . and letter
         if match:
-            return dfs(i+1,j+1)
+            cache[(i,j)] = dfs(i+1,j+1)
+            return cache[(i,j)]
 
+        cache[(i,j)] = False
         return False
 
     # call dfs
     return dfs(0,0)
-    
