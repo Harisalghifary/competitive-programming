b723cd6ff265a5da14dce8c074405da013573615
Masamune Date <haris.solo68@gmail.com>
2024-11-05 23:09:33 +0700

    2914: using string logic

---
+++ leetcode/2914-Minimum-Changes-to-Make-Binary-String-Beautiful.py
@@ -0,0 +1,9 @@
+# Difficulty : Medium
+# Tag : string
+def minChanges(self, s: str) -> int:
+    res = 0
+    for i in range(0, len(s), 2):
+        if s[i]!=s[i+1]:
+            res+=1
+    return res
+    
