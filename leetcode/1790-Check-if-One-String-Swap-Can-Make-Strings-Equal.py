8400d9deb53354b14104b68f88673e57712886e8
Masamune Date <haris.solo68@gmail.com>
2025-02-06 01:09:32 +0700

    1790: using count and compare

--- leetcode/1790-Check-if-One-String-Swap-Can-Make-Strings-Equal.py
+++ leetcode/1790-Check-if-One-String-Swap-Can-Make-Strings-Equal.py
@@ -13,4 +13,21 @@ def areAlmostEqual(self, s1: str, s2: str) -> bool:
     if len(diff)!=2:
         return False
 
-    return s1[diff[0]]==s2[diff[1]] and s1[diff[1]]==s2[diff[0]]               
+    return s1[diff[0]]==s2[diff[1]] and s1[diff[1]]==s2[diff[0]]
+
+# def areAlmostEqual(self, s1: str, s2: str) -> bool:
+#     count = 0
+#     c1 = Counter(s1)
+#     c2 = Counter(s2)
+    
+#     if c1 != c2:
+#         return False
+#     for i in range(len(s1)):
+#         if s1[i]!=s2[i]:
+#             count+=1
+#         if count>2:
+#             return False
+
+#     if count <=2 :
+#         return True
+#     return False               
